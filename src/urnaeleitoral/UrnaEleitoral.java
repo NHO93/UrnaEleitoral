5ae68c2a8077da263c8900ba5b16c30c3faf2abc
Edenilson Fabiano <edenilsonsouza.BSI@gmail.com>
2023-08-24 18:21:49 -0300

    modificado

--- src/urnaeleitoral/UrnaEleitoral.java
+++ src/urnaeleitoral/UrnaEleitoral.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 
 public class UrnaEleitoral {
 
-     /**
+    /**
      * Programa de eleição
      */
-    
+
     public static void main(String[] args) {
-        
+
         Scanner scanner = new Scanner(System.in);
         int totalVotos = 0;
         int votosCandidato1 = 0;
@@ -23,7 +23,7 @@ public class UrnaEleitoral {
         System.out.println("===================");
 
         while (true) {
-           
+
             System.out.println("1. Candidato 1");
             System.out.println("2. Candidato 2");
             System.out.println("3. Candidato 3");
@@ -61,14 +61,14 @@ public class UrnaEleitoral {
         }
         System.out.println("\nu  Resultado");
         System.err.println("======================");
-       
+
         System.out.println("Total de Votos Nulos: " + nulo0 + " votos");
         System.out.println("Total de Votos da Chapa 1: " + votosCandidato1 + " votos");
         System.out.println("Total de Votos da Chapa 2: " + votosCandidato2 + " votos");
         System.out.println("Total de Votos da Chapa 3: " + votosCandidato3 + " votos");
-        System.out.println("Total de Votos:" + totalVotos);
+        System.out.println("Total de Votos: " + totalVotos);
         scanner.close();
 
     }
-    
+
 }
