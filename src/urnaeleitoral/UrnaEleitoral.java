5e95ad5c21a710bbc07c747d00e2b6ccdeba7b09
Edenilson Fabiano <edenilsonsouza.BSI@gmail.com>
2023-08-28 19:57:50 -0300

    Passou por melhorias

--- src/urnaeleitoral/UrnaEleitoral.java
+++ src/urnaeleitoral/UrnaEleitoral.java
@@ -1,81 +1,64 @@
-
 package urnaeleitoral;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class UrnaEleitoral {
 
-    /**
-     * Programa de eleição
-     */
+ 
+   public static void main(String[] args) {
 
-    public static void main(String[] args) {
+       int totalVotos = 0;
+       int votosCandidato1 = 0;
+       int votosCandidato2 = 0;
+       int votosCandidato3 = 0;
+       int nulo = 0;
 
-        Scanner scanner = new Scanner(System.in);
-        
-        int totalVotos = 0;
-        int votosCandidato1 = 0;
-        int votosCandidato2 = 0;
-        int votosCandidato3 = 0;
-        int nulo0 = 0;
-        int senha = 2024;
+    String senha = JOptionPane.showInputDialog(null, "Digite a senha e NÃO ESQUEÇA");
 
-        System.out.println(" ESCOLHA A SUA CHAPA");
-        
-        System.out.println("===================");
-        
-             
-        while (true) {
-
-            System.out.println("1. Candidato 1");
-            
-            System.out.println("2. Candidato 2");
-            
-            System.out.println("3. Candidato 3");
-            
-            System.out.println("0. Nulo 0");
-            
-            System.out.println("Para Encerrar digete a senha: ");
-
-            int escolha = scanner.nextInt();
-
-            if (escolha == senha) {
-                break;
-            } else if (escolha >= 0 && escolha <= 3) {
-                totalVotos++;
-                switch (escolha) {
-                    case 0:
-                        nulo0++;
-                        break;
-                    case 1:
-                        votosCandidato1++;
-                        break;
-                    case 2:
-                        votosCandidato2++;
-                        break;
-                    case 3:
-                        votosCandidato3++;
-                        break;
-                    default:
-                        System.out.println("Escolha invalida. Tente novamente");
-                        break;
+    while (true) {
+        String input = JOptionPane.showInputDialog(null,
+                "Escolha um canditado:\n\n1. Candidato 1\n2. Candidato 2\n3. Candidato 3\n0. Voto nulo\n Insira a senha para encerrar a votação");
+        if (input.equals(senha)) {
+            break;
+        } else {
+            try {
+                int escolha = Integer.parseInt(input);
+                if (escolha >= 0 && escolha <= 3) {
+                    totalVotos++;
+                    switch (escolha) {
+                        case 0:
+                            nulo++;
+                            break;
+                        case 1:
+                            votosCandidato1++;
+                            break;
+                        case 2:
+                            votosCandidato2++;
+                            break;
+                        case 3:
+                            votosCandidato3++;
+                            break;
+                        default:
+                            JOptionPane.showMessageDialog(null, "Escolha inválida. Tente novamente");
+                            break;
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(null, "Escolha inválida. Tente novamente");
                 }
-            } else {
-                System.out.println("Escolha invalida. Tente novamente");
-
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Entrada inválida. Digite um número válido.");
             }
-
         }
-        System.out.println("\nu  Resultado");
-        System.err.println("======================");
+    }
 
-        System.out.println("Total de Votos Nulos: " + nulo0 + " votos");
-        System.out.println("Total de Votos da Chapa 1: " + votosCandidato1 + " votos");
-        System.out.println("Total de Votos da Chapa 2: " + votosCandidato2 + " votos");
-        System.out.println("Total de Votos da Chapa 3: " + votosCandidato3 + " votos");
-        System.out.println("Total de Votos: " + totalVotos);
-        scanner.close();
+    String resultado = "Resultado:\n\n" +
+            "Total de Votos Nulos: " + nulo + " votos\n" +
+            "Total de Votos da Chapa 1: " + votosCandidato1 + " votos\n" +
+            "Total de Votos da Chapa 2: " + votosCandidato2 + " votos\n" +
+            "Total de Votos da Chapa 3: " + votosCandidato3 + " votos\n" +
+            "Total de Votos: " + totalVotos + " votos\n";
 
+    JOptionPane.showMessageDialog(null,resultado);
     }
 
 }
